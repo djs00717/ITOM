bfd3c85a5ff92988d2a49306bf369fc289d6f13f
djs00717 <djs00717@126.com>
2017-11-20 17:36:37 +0800

    itom

---
+++ src/main/java/com/chinadrtv/itom/dao/impl/UserInfoDaoImpl.java
@@ -0,0 +1,24 @@
+package com.chinadrtv.itom.dao.impl;
+
+import com.chinadrtv.itom.dao.UserInfoDao;
+import com.chinadrtv.itom.model.UserInfo;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+/**
+ * Created by djs on 2017/11/15.
+ */
+@Repository
+public class UserInfoDaoImpl implements UserInfoDao {
+
+    public List<UserInfo> list() {
+//        Session session=getSession();
+//        Query query=session.createQuery("from UserInfo");
+//        return query.list();
+        return null;
+    }
+}
--- src/test/com/chinadrtv/itom/dao/impl/UserInfoDaoImplTest.java
+++ src/test/com/chinadrtv/itom/dao/impl/UserInfoDaoImplTest.java
@@ -2,18 +2,23 @@ package com.chinadrtv.itom.dao.impl;
 
 import com.chinadrtv.itom.dao.UserInfoDao;
 import junit.framework.TestCase;
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.chinadrtv.itom.dao.hibernate.HibernateSessionFactory.getSession;
 
 /**
  * Created by djs on 2017/11/14.
  */
-public class UserInfoDaoImplTest extends TestCase {
-    public void testList() throws Exception {
 
-    }
+public class UserInfoDaoImplTest extends TestCase {
+
 
     public static void main(String[] args) {
+
+        System.out.println("");
     }
 
 
